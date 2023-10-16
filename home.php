1cc4ee7c2ffa58b5f3dc4169ab149a64efc1036f
jaderaposa <jaduya678@gmail.com>
2023-10-16 15:01:10 +0800

    asdasdsa

--- home.php
+++ home.php
@@ -10,30 +10,22 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'user';
     <title>Welcome</title>
     <!-- =============================================================================================== -->
     <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
-    <!-- =============================================================================================== -->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!-- =============================================================================================== -->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!-- =============================================================================================== -->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-    <!-- =============================================================================================== -->
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-    <!-- =============================================================================================== -->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-    <!-- =============================================================================================== -->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <!-- =============================================================================================== -->
+    <link rel="stylesheet" href="css/yep.css">
 </head>
 
 <body>
 
     <div class="limiter">
-        <div class="container-login100">
+        <div class="container-login100" style="
+    height: fit-content;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+">
             <div class="wrap-login100">
-                <div class="login100-pic js-tilt" data-tilt>
+                <!-- <div class="login100-pic js-tilt" data-tilt>
                     <img src="images/img-01.png" alt="IMG">
-                </div>
+                </div> -->
 
                 <div class="welcome-message yep">
                     <?php
@@ -41,22 +33,25 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'user';
                         $username = $_SESSION['username'];
                         $role = $_SESSION['role'];
                         echo "Welcome, <b>$username</b>! You are logged in as a $role.<br>";
-    
                     } else {
                         // Handle the case where the session data is not set.
                         echo "Session data not found. Please log in first.";
                     }
+                    ?>
 
-                    if ($role === 'admin') {
-                        echo '<table>';
-                        echo 
-                        '
+                    <!-- Add some space with CSS margin -->
+                    <div class="space"></div>
+
+                    <div>
+                        <?php
+                        if ($role === 'admin') {
+                            echo '<table>';
+                            echo '
                         <tr>
                             <th>Staffs</th>
                         </tr>';
-                        // Add table content here for the staff table
-                        echo 
-                        '<tr>
+                            echo '
+                        <tr>
                             <td>Maam Janine</td>
                         </tr>
                         <tr>
@@ -65,21 +60,18 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'user';
                         <tr>
                             <td>Sir Reilan</td>
                         </tr>
-                        '
-                        ;
-                        // Repeat for additional rows
-                        echo '</table>';
-                    }
+                        ';
+                            echo '</table>';
+                        }
 
-                    if ($role === 'admin' || $role === 'staff') {
-                        echo '<table>';
-                        echo '
+                        if ($role === 'admin' || $role === 'staff') {
+                            echo '<table>';
+                            echo '
                         <tr>
                             <th>User</th>
                         </tr>
                         ';
-                        // Add table content here for the user table
-                        echo '
+                            echo '
                         <tr>
                             <td>Jade Raposa</td>
                         </tr>
@@ -90,23 +82,19 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'user';
                             <td>Richy Rich</td>
                         </tr>
                         ';
-                        // Repeat for additional rows
-                        echo '</table>';
-                    }
+                            echo '</table>';
+                        }
 
-                    if ($role === 'user') {
-                        echo '<table>';
-                        echo '<p>You cannot use this because you are a student</p>';
-                        // Repeat for additional rows
-                        echo '</table>';
-                    }
-                    ?>
-
+                        if ($role === 'user') {
+                            echo '<p>You cannot use this because you are a student</p>';
+                        }
+                        ?>
+                    </div>
                 </div>
 
                 <div class="container">
                     <a href="functions/logout.php">
-                        <i class="fa fa-long-arrow-left m-l-5" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Log Out
+                        <i class="fa fa-long-arrow-left m-l-5" aria-hidden="true"></i>Log Out
                     </a>
                 </div>
             </div>
@@ -115,28 +103,16 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'user';
 
     <style>
         .container {
-            position: relative;
+            position: absolute;
             display: flex;
             justify-content: end;
+            top: 0;
+            right: 0;
+            margin-right: 1rem;
+            margin-top: 1rem;
         }
     </style>
 
-    <!-- =============================================================================================== -->
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-    <!-- =============================================================================================== -->
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <!-- =============================================================================================== -->
-    <script src="vendor/select2/select2.min.js"></script>
-    <!-- =============================================================================================== -->
-    <script src="vendor/tilt/tilt.jquery.min.js"></script>
-    <script>
-        $('.js-tilt').tilt({
-            scale: 1.1
-        })
-    </script>
-    <!-- =============================================================================================== -->
-    <script src="js/main.js"></script>
 
 </body>
 
